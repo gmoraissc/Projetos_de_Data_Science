eacb00055a959b0a61cd4fb51877d9385f228809
gmoraissc <63673090+gmoraissc@users.noreply.github.com>
2021-11-28 16:48:09 -0300

    Create Pipeline.py

---
+++ Pipeline.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+X, y = make_classification(random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+pipe = make_pipeline(StandardScaler(), LogisticRegression())
+pipe.fit(X_train, y_train)  
+
+pipe.score(X_test, y_test)
+#0.96
+
+
